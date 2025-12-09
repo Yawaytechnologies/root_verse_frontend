3309c3abf3ff6fcd0c10c16020795a705f1c8069
sathish-dev <sathish@yawaytech.com>
2025-12-09 10:48:23 +0530

    base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './global.css'
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <div className="text-4xl font-bold text-blue-500 underline">
+        Tailwind Check ✅
+      </div>
+    </div>
+  )
+}
+
+export default App
